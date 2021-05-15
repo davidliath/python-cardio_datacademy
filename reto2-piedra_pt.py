b7446e45faa7f84926518ca67f6b7402ec2f4944
David Rosas <davidrosasv43@gmail.com>
2021-05-15 14:12:41 -0700

    Funcion juego creada. Reto 2 cumplido

--- reto2-piedra_pt.py
+++ reto2-piedra_pt.py
@@ -1,3 +1,32 @@
+def juego(j1, j2):
+
+    gana1 = 'El ganador es el jugador 1.'
+    gana2 = 'El ganador es el jugador 2.'
+    empate = "Empate. Juegen otra vez."
+    
+    if j1 == 'piedra':
+        if j2 == 'papel':
+            print(gana2)
+        elif j2 == 'tijera':
+            print(gana1)
+        else:
+            print(empate)
+    if j1 == 'papel':
+        if j2 == 'tijera':
+            print(gana2)
+        elif j2 == 'piedra':
+            print(gana1)
+        else:
+            print(empate)
+    if j1 == 'tijera':
+        if j2 == 'piedra':
+            print(gana2)
+        elif j2 == 'papel':
+            print(gana1)
+        else:
+            print(empate)
+
+
 def run():
     print('Con este programa podrás jugar ¡Piedra, Papel o Tijera!')
 
