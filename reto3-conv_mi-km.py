cde07ceb5acb79cb6024c407249d974a602aa5d0
David Rosas <davidrosasv43@gmail.com>
2021-05-15 14:28:15 -0700

    Función conversor creada. Reto 3 cumplido

--- reto3-conv_mi-km.py
+++ reto3-conv_mi-km.py
@@ -1,6 +1,11 @@
+def conversor(mi):
+    km = mi * 1.609344
+    print(str(mi) + ' mi  =  ' + str(km) + ' km.')
+
+
 def run():
     print('Conversor de Millas a Kilómetros.')
-    m = int(input('Millas: '))
+    m = float(input('Millas: '))
 
     conversor(m)
 
