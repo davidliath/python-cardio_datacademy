51d6f408c5049254523a6d315af3b4337178109d
David Rosas <davidrosasv43@gmail.com>
2021-05-15 15:52:01 -0700

    Programa elemental creado en run()

--- reto5-rangs_camb.py
+++ reto5-rangs_camb.py
@@ -1,6 +1,16 @@
-def run():
+def comparacion(inf, sup):
     pass
 
+
+def run():
+    print('En este programa jugaremos a dar un número que se encuentre dentro de cierto rango dado.\nPara esto, primero estableceremos el rango dando un límite inferior y uno superior:')
+
+    inferior = float(input('Límite inferior = '))
+    superior = float(input('Límite superior = '))
+
+    print('Ahora sí, da un número para checar si está dentro del rango:')
+
+    comparacion(inferior, superior)
 
 if __name__ == '__main__':
     run()
