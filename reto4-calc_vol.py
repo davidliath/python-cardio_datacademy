a13cc901c4dbb4504b4a91720114c75613f5afc7
David Rosas <davidrosasv43@gmail.com>
2021-05-15 14:39:09 -0700

    Programa básico creado en run()

--- reto4-calc_vol.py
+++ reto4-calc_vol.py
@@ -1,5 +1,10 @@
 def run():
-    pass
+    print('Esta es una calculadora de volúmenes de cilindros.\nDame el radio y la altura del cilindro para calcularlo:')
+
+    radio = float(input('Radio (cm)    = '))
+    altura = float(input('Altura (cm)   = '))
+    volumen_cil(radio, altura)
+
 
 
 if __name__ == '__main__':
