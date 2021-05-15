f9a0186e5d4e62ac648c4b959bc48034bd27a405
David Rosas <davidrosasv43@gmail.com>
2021-05-15 13:19:41 -0700

    Reto cumplido

--- reto1-area_tri.py
+++ reto1-area_tri.py
@@ -1,6 +1,18 @@
+def areatriangulo(base,altura):
+    area = (base * altura) / 2
+    return area
+
+
 def run():
-    pass
+    print('Este programa calcula el área de un triángulo, dadas su base y altura.')
+
+    b = int(input('Base: '))
+    h = int(input('Altura: '))
+
+    a = areatriangulo(b,h)
+    
+    print('El area de tu tiángulo es: \n' + str(a) + ' unidades cuadradas.')
 
 
-def __name__ == '__main__':
+if __name__ == '__main__':
     run()
