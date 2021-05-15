58d8a54e069f12f00e9aeb0003880c16b4545db0
David Rosas <davidrosasv43@gmail.com>
2021-05-15 14:55:45 -0700

    Función volumen_cil() creada. Reto 4 cumplido

--- reto4-calc_vol.py
+++ reto4-calc_vol.py
@@ -1,10 +1,18 @@
+from math import pi
+
+
+def volumen_cil(r, h):
+    area_circulo = pi * r**2
+    vol = area_circulo * h
+    print('V_cilindro = ' + str(vol) + ' cm^2')
+
+
 def run():
     print('Esta es una calculadora de volúmenes de cilindros.\nDame el radio y la altura del cilindro para calcularlo:')
 
     radio = float(input('Radio (cm)    = '))
     altura = float(input('Altura (cm)   = '))
     volumen_cil(radio, altura)
-
 
 
 if __name__ == '__main__':
